ab3edc73e7a26fa07c0c927ed219872cda7b7ba0
umair khan <asadsher2324@gmail.com>
2024-05-30 11:45:02 -0700

    intial update

---
+++ emrimart/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'pe3nkb23',
+    dataset: 'production'
+  }
+})
---
+++ emrimart/schemaTypes/index.ts
@@ -0,0 +1,4 @@
+import product from "./product";
+import category from "./category";
+import heroImage from "./heroImage";
+export const schemaTypes = [product, category, heroImage];
